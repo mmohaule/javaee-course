2bb0787686f68e89647e253c9fe8fe7ee01e8339
mmohaule <mtoxlee@gmail.com>
2019-03-14 09:26:23 +0200

    created web5

---
+++ web5/src/com/airline/controller/AddPassenger.java
@@ -0,0 +1,31 @@
+package com.airline.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AddPassenger")
+public class AddPassenger extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AddPassenger() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/add_passenger.jsp");
+		
+		view.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
