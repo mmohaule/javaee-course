ae123c567ac74349761f4c06da2ae405bc62e22b
mmohaule <mtoxlee@gmail.com>
2019-03-15 14:56:59 +0200

    list size

--- web11/src/com/airline/controller/MainPage.java
+++ web11/src/com/airline/controller/MainPage.java
@@ -2,12 +2,17 @@ package com.airline.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.airline.models.Passenger;
 
 @WebServlet("")
 public class MainPage extends HttpServlet {
@@ -22,7 +27,12 @@ public class MainPage extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		
 		response.setContentType("text/html");
-		out.println("Passenger has been added");
+		
+		ServletContext sc = this.getServletContext();
+		@SuppressWarnings("unchecked")
+		List<Passenger> passengers = (ArrayList<Passenger>) sc.getAttribute("passengers");
+		out.println("New Passenger has been added");
+		out.println("<p>List Size: " + passengers.size() + "</p>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
